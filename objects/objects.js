b14464128d9eb0db1dd22f35d0c3a27da2344b2e
Striker-sp <70858012+Striker-sp@users.noreply.github.com>
2024-06-14 14:32:46 +0530

    Add files via upload

---
+++ objects/objects.js
@@ -0,0 +1,11 @@
+const user = {
+    firstname : "Harley",
+    lastname : "Davidson",
+    email : "harleydavidson@motor.com"
+
+}
+
+console.log(user["firstname"]);
+console.log(user["lastname"]);
+console.log(user["email"]);
+    
